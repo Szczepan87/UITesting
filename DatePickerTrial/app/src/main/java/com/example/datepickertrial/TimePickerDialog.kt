e83a755a6be01857d95bbd19e143871f22098890
Lukasz Szczepanski <lukasz.szczepanski@hemmersbach.com>
2019-05-27 11:10:00 +0200

    time picker works

---
+++ DatePickerTrial/app/src/main/java/com/example/datepickertrial/TimePickerDialog.kt
@@ -0,0 +1,25 @@
+package com.example.datepickertrial
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.util.Log
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+class TimePickerDialog: DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val cal = Calendar.getInstance()
+        val hour = cal.get(Calendar.HOUR_OF_DAY)
+        val minute = cal.get(Calendar.MINUTE)
+
+        return TimePickerDialog(activity, this, hour, minute, true)
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        Log.d("TIME", "Hour: $hourOfDay \n Minute: $minute")
+    }
+}
